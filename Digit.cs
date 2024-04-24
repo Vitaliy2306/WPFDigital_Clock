bc0facb29668da6c48c7826feb59346d20cef6c3
Vitaliy2306 <126496316+Vitaliy2306@users.noreply.github.com>
2024-04-24 20:44:22 +0200

    Add files via upload

---
+++ Digit.cs
@@ -0,0 +1,559 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+
+
+namespace WpfDigitalClock
+{
+    public class Digit : Panel
+    {
+        
+
+        // Ширина цифри без фаски (скошених елементів) і проміжків.
+        double widthBlankSegment = 100;
+
+        // Висота квадрату верхньої чи нижньої половинок цифри.
+        // Цифра складається із верхньої і нижньої половинок.
+        public double heightBlankSegment = 100;
+
+        // Товщина сегментів - обрізаних  полігонів.
+        double thicknessSegment = 30;
+
+        // Зміщення точок ліній суміжних полігонів
+        // до центрального полігону (елементу).
+        // Для керування товщини центрального сегмента
+        double correctCenter = 10;
+
+        // Зміщення сегментів цифри один від одного,
+        // значення змінює проміжок між сегментами. 
+        double _strokeThickness = 1;
+        public double StrokeThickness
+        {
+            get { return _strokeThickness; }
+            set { _strokeThickness = value; }
+        }
+
+        // Колір сегментів цифри.
+        SolidColorBrush _colorDigit = Brushes.Black;
+        public SolidColorBrush ColorDigit
+        {
+            get { return _colorDigit; }
+            set
+            {
+                _colorDigit = value;
+                // Якщо в XAML було скидання чи в інших випадках,
+                // то відновлюємо пензлик за замовчуванням.
+                if (value == null) _colorDigit = Brushes.Black;
+
+                // Перемальовуємо сегменти.
+                DrawDigit(0, 0);
+            }
+        }
+
+        
+
+        public Digit() : base()
+        {
+            // Розмір цифри за замовчуванням
+            Width = 100;
+
+            // Иніціалізація полігонів - сегментів цифри,
+            // і додавання їх в колекцію дочірніх сегментів 
+            // батьківської панелі.
+            for (int i = 0; i < 7; i++)
+            {
+                Children.Add(new Polygon());
+            }
+        }
+
+        
+
+
+        
+
+        // Візуалізація значень цифри.
+        public void ValueDigit(int digit)
+        {
+            switch (digit)
+            {
+                case 0:
+                    // Ліве
+                    Children[0].Opacity = 1;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 0;
+                    // Ліве знизу
+                    Children[4].Opacity = 1;
+                    // Нижнє
+                    Children[5].Opacity = 1;
+                    // Праве знизу
+                    Children[6].Opacity = 1;
+
+                    break;
+                case 1:
+                    // Ліве
+                    Children[0].Opacity = 0;
+                    // Верхнє
+                    Children[1].Opacity = 0;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 0;
+                    // Ліве знизу
+                    Children[4].Opacity = 0;
+                    // Нижнє
+                    Children[5].Opacity = 0;
+                    // Праве знизу
+                    Children[6].Opacity = 1;
+
+                    break;
+                case 2:
+                    // Ліве
+                    Children[0].Opacity = 0;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 1;
+                    // Ліве знизу
+                    Children[4].Opacity = 1;
+                    // Нижнє
+                    Children[5].Opacity = 1;
+                    // Праве знизу
+                    Children[6].Opacity = 0;
+                    break;
+                case 3:
+                    // Ліве
+                    Children[0].Opacity = 0;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 1;
+                    // Ліве знизу
+                    Children[4].Opacity = 0;
+                    // Нижнє
+                    Children[5].Opacity = 1;
+                    // Праве знизу
+                    Children[6].Opacity = 1;
+                    break;
+                case 4:
+                    // Ліве
+                    Children[0].Opacity = 1;
+                    // Верхнє
+                    Children[1].Opacity = 0;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 1;
+                    // Ліве нижнє
+                    Children[4].Opacity = 0;
+                    // Нижнє
+                    Children[5].Opacity = 0;
+                    // Праве нижнє
+                    Children[6].Opacity = 1;
+                    break;
+                case 5:
+                    // Ліве
+                    Children[0].Opacity = 1;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 0;
+                    // Центральне
+                    Children[3].Opacity = 1;
+                    // Ліве верхнє
+                    Children[4].Opacity = 0;
+                    // Нижнє
+                    Children[5].Opacity = 1;
+                    // Праве верхнє
+                    Children[6].Opacity = 1;
+                    break;
+                case 6:
+                    // Ліве
+                    Children[0].Opacity = 1;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 0;
+                    // Центральне
+                    Children[3].Opacity = 1;
+                    // Ліве нижнє
+                    Children[4].Opacity = 1;
+                    // Нижнє
+                    Children[5].Opacity = 1;
+                    // Праве нижнє
+                    Children[6].Opacity = 1;
+                    break;
+                case 7:
+                    // Ліве
+                    Children[0].Opacity = 0;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Центральне
+                    Children[3].Opacity = 0;
+                    // Ліве нижнє
+                    Children[4].Opacity = 0;
+                    // Нижнє
+                    Children[5].Opacity = 0;
+                    // Праве нижнє
+                    Children[6].Opacity = 1;
+                    break;
+                case 8:
+                    // Ліве
+                    Children[0].Opacity = 1;
+                    // Верхнє
+                    Children[1].Opacity = 1;
+                    // Праве
+                    Children[2].Opacity = 1;
+                    // Center
+                    Children[3].Opacity = 1;
+                    // LeftBottom
+                    Children[4].Opacity = 1;
+                    // Bottom
+                    Children[5].Opacity = 1;
+                    // RightBottom
+                    Children[6].Opacity = 1;
+                    break;
+                case 9:
+                    // Left
+                    Children[0].Opacity = 1;
+                    // Top
+                    Children[1].Opacity = 1;
+                    // Right
+                    Children[2].Opacity = 1;
+                    // Center
+                    Children[3].Opacity = 1;
+                    // LeftBottom
+                    Children[4].Opacity = 0;
+                    // Bottom
+                    Children[5].Opacity = 1;
+                    // RightBottom
+                    Children[6].Opacity = 1;
+                    break;
+            }
+
+
+
+        }
+
+    
+
+
+        #region Вычисление размеров для сегментов цифры
+
+        private void ComputeSize()
+        {
+            // Возможно при сбросе ширины в XAML дизайнере.
+            if (double.IsNaN(Width) == true) Width = 100;
+
+            // Вычисление ширины цифры на основе заданной пользователем общей ширины.
+            widthBlankSegment = Width - (2 * thicknessSegment / 3 + _strokeThickness * 2);
+
+            // Высота сегментов половинки и ширина цифры всегда одинаковые.
+            // Цифра состоит из верхней половинки и нижней половинки.
+            heightBlankSegment = widthBlankSegment;
+
+            // Корректировка толщины центрального сегмента,
+            // для уравнивания с толщинами других сегментов.
+            correctCenter = widthBlankSegment / 8;
+
+            // Корректировка толщины сегментов цифры.
+            thicknessSegment = widthBlankSegment / 4;
+
+            // Вычисление общей высоты цифры с верхней и нижней половинками.
+            Height = heightBlankSegment * 2 + 2 * thicknessSegment / 3 + _strokeThickness * 2 + _strokeThickness * 2;
+
+            // Рисование полной цифры с новыми размерами.
+            DrawDigit(0, 0);
+        }
+
+        #endregion
+
+
+        #region Рисование графики элементов цифры
+
+        void DrawDigit(double x, double y)
+        {
+            x = x +
+                // Смещение вправо на фаску левых сегментов
+                thicknessSegment / 3 +
+                // Смещение вправо на толщину промежутка
+                // (смещение левых сегментов вправо для создания промежутка).
+                _strokeThickness;
+
+            // Смещение по высоте на фаску сегмента и
+            // межсегментного промежутка (смещения верхнего элемента вверх для создания промежутка между сегментами).
+            y = y + thicknessSegment / 3 + _strokeThickness;
+
+            // Рисование сегментов цифры.
+            SegmentLeft(x, y, _colorDigit, _strokeThickness);
+            SegmentTop(x, y, _colorDigit, _strokeThickness);
+            SegmentRight(x, y, _colorDigit, _strokeThickness);
+            SegmentCenter(x, y, _colorDigit, _strokeThickness);
+            SegmentLeftBottom(x, y, _colorDigit, _strokeThickness);
+            SegmentBottom(x, y, _colorDigit, _strokeThickness);
+            SegmentRightBottom(x, y, _colorDigit, _strokeThickness);
+        }
+
+        void SegmentLeft(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[0];
+            PointCollection Points = new()
+            {
+				// left
+				new System.Windows.Point(x - widthBlankSegment / 2, y + heightBlankSegment / 2),
+				// top
+				new Point(x, y),
+				// right
+				new Point(x + widthBlankSegment / 2, y + heightBlankSegment / 2),
+				// right2
+				new Point(x + widthBlankSegment / 2, y + heightBlankSegment / 2 + correctCenter /*корректирвка центрального*/),
+				// bottom
+				new Point(x, y + heightBlankSegment)
+            };
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                X = -strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            // Обрезание прямоугольного полигона до трапеции.
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x - thicknessSegment / 3, y, thicknessSegment, heightBlankSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        void SegmentTop(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[1];
+            PointCollection Points = new()
+            {
+                new Point(x + widthBlankSegment / 2, y - heightBlankSegment / 2),
+                new Point(x + widthBlankSegment, y),
+                new Point(x + widthBlankSegment / 2, y + heightBlankSegment / 2),
+                new Point(x, y)
+            };
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                Y = -strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x, y - thicknessSegment / 3, widthBlankSegment, thicknessSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        void SegmentRight(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[2];
+            PointCollection Points = new()
+            {
+                // left-correct
+                new Point(x + widthBlankSegment / 2,y + heightBlankSegment / 2 + correctCenter),
+                // left
+                new Point(x + widthBlankSegment / 2,y + heightBlankSegment / 2),
+                // top
+                new Point(x + widthBlankSegment, y),
+                // right
+                new Point(x + widthBlankSegment*1.5, y + heightBlankSegment / 2),
+
+                new Point(x + widthBlankSegment, y + heightBlankSegment)
+            };
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                X = strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x - thicknessSegment / 1.5 + widthBlankSegment, y, thicknessSegment, heightBlankSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        void SegmentCenter(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[3];
+            PointCollection Points = new()
+            {
+                // top
+                new Point(x + widthBlankSegment/2, y + heightBlankSegment / 2 + correctCenter /*корректирвка центрального*/),
+                new Point(x + widthBlankSegment, y + heightBlankSegment),
+                // bottom
+                new Point(x + widthBlankSegment/2, y + heightBlankSegment*1.5 - correctCenter /*корректирвка центрального*/),
+                new Point(x, y + heightBlankSegment)
+            };
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                Y = strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x, y + heightBlankSegment - thicknessSegment / 2, widthBlankSegment, thicknessSegment)
+            };
+
+            pg.Clip = rg;
+        }
+
+        void SegmentLeftBottom(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[4];
+            PointCollection Points = new()
+            {
+                // left
+                new Point(x - widthBlankSegment / 2, y + heightBlankSegment *  1.5),
+                // top
+                new Point(x, y + heightBlankSegment),
+                // right correct
+                new Point(x + widthBlankSegment / 2, y - correctCenter + heightBlankSegment *  1.5),
+                // right
+                new Point(x + widthBlankSegment / 2, y + heightBlankSegment *  1.5),
+                // bottom
+                new Point(x, y + heightBlankSegment * 2)
+            };
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                X = -strokeThickness,
+                Y = 2 * strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x - thicknessSegment / 3, y + heightBlankSegment, thicknessSegment, heightBlankSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        void SegmentBottom(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[5];
+            PointCollection Points = new()
+            {
+                new Point(x + widthBlankSegment / 2, y  + heightBlankSegment * 1.5) ,
+                new Point(x + widthBlankSegment, y + heightBlankSegment * 2) ,
+                new Point(x + widthBlankSegment / 2, y + heightBlankSegment * 2.5) ,
+                new Point(x, y + heightBlankSegment * 2)
+            };
+
+            pg.Points = Points;
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                Y = 3 * strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x, y - thicknessSegment / 1.5 + heightBlankSegment * 2, widthBlankSegment, thicknessSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        void SegmentRightBottom(double x, double y, SolidColorBrush color, double strokeThickness)
+        {
+            Polygon pg = (Polygon)Children[6];
+            pg.Points = new PointCollection()
+            {
+                // left 
+                new Point(x + widthBlankSegment / 2, y + heightBlankSegment * 1.5) ,
+                // left correct
+                new Point(x + widthBlankSegment / 2, y + heightBlankSegment * 1.5 - correctCenter) ,
+                new Point(x + widthBlankSegment, y + heightBlankSegment) ,
+                new Point(x + widthBlankSegment * 1.5, y + heightBlankSegment * 1.5) ,
+                new Point(x + widthBlankSegment, y + heightBlankSegment * 2)
+            };
+            pg.Fill = color;
+
+            TranslateTransform tt = new()
+            {
+                X = strokeThickness,
+                Y = 2 * strokeThickness
+            };
+            pg.RenderTransform = tt;
+
+            RectangleGeometry rg = new()
+            {
+                Rect = new Rect(x - thicknessSegment / 1.5 + widthBlankSegment, y + heightBlankSegment, thicknessSegment, heightBlankSegment)
+            };
+            pg.Clip = rg;
+        }
+
+        #endregion
+
+
+        #region Переопределенные  методы класса Panel
+
+        
+        // Измерение дочерних элементов
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size panelDesiredSize = new();
+
+            foreach (UIElement child in InternalChildren)
+            {
+                
+                child.Measure(availableSize);
+                panelDesiredSize = child.DesiredSize;
+            }
+
+            return panelDesiredSize;
+        }
+
+
+        // Размещение дочерних элементов
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (UIElement child in InternalChildren)
+            {
+                
+                child.Arrange(new Rect(new Point(), child.DesiredSize));
+            }
+
+            // Вычисление размеров всей цифры в зависимости от ширины панели.
+            ComputeSize();
+
+            return finalSize;
+        }
+
+        #endregion
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+
+namespace WpfDigitalClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Таймер слідкування за поточним часом.
+        readonly DispatcherTimer dispatcherTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Иніціалізація і запуск таймера.
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            dispatcherTimer.Start();
+        }
+
+
+        
+
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            ShowDateTime();
+        }
+
+
+        // Лічильник тем
+        int countTheme = 0;
+        // Вибір кольорових тем
+        private void BorderColors_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            (SolidColorBrush color, SolidColorBrush bg)  = ThemeGenerator(countTheme);
+
+            ThemeClock(color, bg);
+
+            countTheme++;
+            if (countTheme == 6) countTheme = 0;
+        }
+
+
+        // Зміщення вікна захоплюючи клієнтську частину
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+
+        // Перемикання в пвноекранний режим і навпаки.
+        private void Window_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+
+        // Закриття вікна
+        private void BorderClose_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Відступ головного контейнера для цифр 2,5% від ширини вікна.
+            maingrid.Margin = new Thickness(this.Width * 0.025);
+        }
+
+      
+
+
+        
+
+        // Отримання поточного часу і дати.
+        // Вивід на табло.
+        void ShowDateTime()
+        {
+            // Отримання і виведення часу 
+            string time = DateTime.Now.ToString("HHmmss");
+            int h1 = int.Parse(time.ToString()[0].ToString());
+            int h2 = int.Parse(time.ToString()[1].ToString());
+            int m1 = int.Parse(time.ToString()[2].ToString());
+            int m2 = int.Parse(time.ToString()[3].ToString());
+            int s1 = int.Parse(time.ToString()[4].ToString());
+            int s2 = int.Parse(time.ToString()[5].ToString());
+            digit1.ValueDigit(h1);
+            digit2.ValueDigit(h2);
+            digit3.ValueDigit(m1);
+            digit4.ValueDigit(m2);
+            digit5.ValueDigit(s1);
+            digit6.ValueDigit(s2);
+
+            // Отримання і виведення дати
+            string date = DateTime.Now.ToString("ddMMyyyy");
+            int d1 = int.Parse(date.ToString()[0].ToString());
+            int d2 = int.Parse(date.ToString()[1].ToString());
+            int M1 = int.Parse(date.ToString()[2].ToString());
+            int M2 = int.Parse(date.ToString()[3].ToString());
+            int y1 = int.Parse(date.ToString()[4].ToString());
+            int y2 = int.Parse(date.ToString()[5].ToString());
+            int y3 = int.Parse(date.ToString()[6].ToString());
+            int y4 = int.Parse(date.ToString()[7].ToString());
+            digitD1.ValueDigit(d1);
+            digitD2.ValueDigit(d2);
+            digitM1.ValueDigit(M1);
+            digitM2.ValueDigit(M2);
+            digitY1.ValueDigit(y1);
+            digitY2.ValueDigit(y2);
+            digitY3.ValueDigit(y3);
+            digitY4.ValueDigit(y4);
+
+            // Отримання і виведення дня тижня
+            string day = DateTime.Now.ToString("ddd");
+            labelddd.Content = day;
+        }
+
+
+        // призначення кольорових тем цифрового годинника.
+        void ThemeClock(SolidColorBrush color, SolidColorBrush bg)
+        {
+            // Фон, табло годинника.
+            this.Background = bg;
+
+
+            // Цифри табло
+            digit1.ColorDigit = color;
+            digit2.ColorDigit = color;
+            digit3.ColorDigit = color;
+            digit4.ColorDigit = color;
+            digit5.ColorDigit = color;
+            digit6.ColorDigit = color;
+            digitD1.ColorDigit = color;
+            digitD2.ColorDigit = color;
+            digitM1.ColorDigit = color;
+            digitM2.ColorDigit = color;
+            digitY1.ColorDigit = color;
+            digitY2.ColorDigit = color;
+            digitY3.ColorDigit = color;
+            digitY4.ColorDigit = color;
+
+            // Розподільчі точки (еліпси)
+            dotTime1.Fill = color;
+            dotTime2.Fill = color;
+            dotDate1.Fill = color;
+            dotDate2.Fill = color;
+
+            // Розподільчі лінії.
+            lineSeparator.Stroke = color;
+            lineControl.Stroke = color;
+
+            // Кнопка зміни кольору
+            borderColors.BorderBrush = color;
+            ellipseColors1.Stroke = color;
+            ellipseColors1.Fill = bg;
+            ellipseColors2.Stroke = color;
+            ellipseColors2.Fill = color;
+
+            // Кнопка закриття
+            borderClose.BorderBrush = color;
+            lineClose1.Stroke = color;
+            lineClose2.Stroke = color;
+
+            // Колір текстових міток.
+            labelddd.Foreground = color;
+            labelIP.Foreground = color;
+
+            // Смайлик (Path) (Заради жарту)
+            smile.Stroke = color;
+        }
+
+
+        // Генератор тем,
+        // на виходе выдає колір графічних елементів і
+        // колір фону дисплею годинника.
+        public static (SolidColorBrush color, SolidColorBrush bg) ThemeGenerator(int count) => count switch
+        {
+            0 => (Brushes.Red, Brushes.Black),
+            1 => (new SolidColorBrush(Color.FromRgb(70, 255, 70)), Brushes.Black),
+            2 => (new SolidColorBrush(Color.FromRgb(30, 150, 255)), Brushes.Black),
+            3 => (Brushes.Yellow, Brushes.Black),
+            4 => (Brushes.White, Brushes.Black),
+            _ => (Brushes.Black, new SolidColorBrush(Color.FromRgb(118, 167, 148)))
+        };
+
+
+        
+
+    }
+}
